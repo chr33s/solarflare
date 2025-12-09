a57edbb821ff0539860d3ac056a7825e8dcb0886
chris <chr33s@icloud.com>
2025-12-09 11:11:42 +0700

    add: AbortController

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -5,31 +5,50 @@ export default {
   async fetch(request: Request, env: Env) {
     const url = new URL(request.url)
 
+    const abortController = new AbortController()
+    request.signal.addEventListener('abort', () => abortController.abort())
+    const init = { signal: abortController.signal }
+
     if (url.pathname !== "/") {
       console.log({ asset: url.pathname })
-      const asset = await env.ASSETS.fetch(request.url)
+      const asset = await env.ASSETS.fetch(request.url, init)
       if (asset.ok) return asset
     }
 
     console.log({ fallback: "index.html" })
-    const asset = await env.ASSETS.fetch(new URL("index.html", url))
+    const asset = await env.ASSETS.fetch(new URL("index.html", url), init)
     const template = await asset.text()
-
     const [before, after] = template.split(/<div id="app">(?:\$1)?<\/div>/)
-    const app = renderToReadableStream(<App />)
 
     const encoder = new TextEncoder()
     const stream = new ReadableStream({
+      cancel() {
+        abortController.abort()
+      },
       async start(controller) {
-        controller.enqueue(encoder.encode(before + '<div id="app">'))
-        const reader = app.getReader()
-        while (true) {
-          const { done, value } = await reader.read()
-          if (done) break
-          controller.enqueue(typeof value === 'string' ? encoder.encode(value) : value)
+        try {
+          controller.enqueue(encoder.encode(before + '<div id="app">'))
+          const reader = app.getReader()
+          while (true) {
+            if (abortController.signal.aborted) {
+              reader.cancel()
+              controller.close()
+              return
+            }
+
+            const { done, value } = await reader.read()
+            if (done) break
+            controller.enqueue(typeof value === 'string' ? encoder.encode(value) : value)
+          }
+          controller.enqueue(encoder.encode('</div>' + after))
+          controller.close()
+        } catch (error) {
+          if (abortController.signal.aborted) {
+            controller.close()
+          } else {
+            controller.error(error)
+          }
         }
-        controller.enqueue(encoder.encode('</div>' + after))
-        controller.close()
       }
     })
 
