09dbfcc14c7cb39f6bc6b407a9440015de53dd5e
chris <chr33s@icloud.com>
2025-12-09 11:11:22 +0700

    update: buffering -> streaming

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,4 +1,4 @@
-import { renderToString } from 'preact-render-to-string'
+import { renderToReadableStream } from 'preact-render-to-string/stream'
 import { App } from './app'
 
 export default {
@@ -15,13 +15,25 @@ export default {
     const asset = await env.ASSETS.fetch(new URL("index.html", url))
     const template = await asset.text()
 
-    const app = renderToString(<App />)
-    const html = template.replace(
-      /<div id="app">(?:\$1)?<\/div>/,
-      `<div id="app">${app}</div>`
-    )
+    const [before, after] = template.split(/<div id="app">(?:\$1)?<\/div>/)
+    const app = renderToReadableStream(<App />)
 
-    return new Response(html, {
+    const encoder = new TextEncoder()
+    const stream = new ReadableStream({
+      async start(controller) {
+        controller.enqueue(encoder.encode(before + '<div id="app">'))
+        const reader = app.getReader()
+        while (true) {
+          const { done, value } = await reader.read()
+          if (done) break
+          controller.enqueue(typeof value === 'string' ? encoder.encode(value) : value)
+        }
+        controller.enqueue(encoder.encode('</div>' + after))
+        controller.close()
+      }
+    })
+
+    return new Response(stream, {
       headers: { 'Content-Type': 'text/html; charset=utf-8' },
     })
   },
