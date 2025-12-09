4f8fd7023d515ab2172b6f7ab6b678cd4b2d383c
chris <chr33s@icloud.com>
2025-12-09 10:46:37 +0700

    update: vite to bun

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,18 +1,28 @@
-import { renderToReadableStream } from 'preact-render-to-string/stream'
+import { renderToString } from 'preact-render-to-string'
 import { App } from './app'
 
-export async function render(uri: string) {
-	console.log({ uri })
-  const html = renderToReadableStream(<App />)
-  return { html }
-}
-
 export default {
-	async fetch(request: Request) {
-		console.log({ "fetch": request.url });
-		const { html } = await render(new URL(request.url).pathname);
-		return new Response(html, {
-			headers: { 'Content-Type': 'text/html; charset=utf-8' },
-		});
-	}
-} satisfies ExportedHandler;
+  async fetch(request: Request, env: Env) {
+    const url = new URL(request.url)
+
+    if (url.pathname !== "/") {
+      console.log({ asset: url.pathname })
+      const asset = await env.ASSETS.fetch(request.url)
+      if (asset.ok) return asset
+    }
+
+    console.log({ fallback: "index.html" })
+    const asset = await env.ASSETS.fetch(new URL("index.html", url))
+    const template = await asset.text()
+
+    const app = renderToString(<App />)
+    const html = template.replace(
+      /<div id="app">(?:\$1)?<\/div>/,
+      `<div id="app">${app}</div>`
+    )
+
+    return new Response(html, {
+      headers: { 'Content-Type': 'text/html; charset=utf-8' },
+    })
+  },
+} satisfies ExportedHandler<Env>
